6c615fb6048691fc4594338957f17242658a0bd1
William Ulff <william.ulff@koncepta.se>
2026-02-04 13:24:37 +0100

    upp

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       <div className="h-screen"></div>
       <div className="w-full h-screen flex wrap flex-col gap-5 items-center justify-center">
         <h1 className="text-xl">Elektra</h1>
-        <p className="text-xs text-center">
+        <p className="text-xs text-center p-5">
           Alla hjärtans dag grejen kändes gjord. Och sen har jag redan frågat om
           du vill vara min den helgen. Så här har du en knapp som ger dig en
           slumpad tvättbjörn. Puss.
